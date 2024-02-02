b1239de6ee8711327f5ac64b08f372aa5f4f545b
Irfan khan <80601941+ik-deep@users.noreply.github.com>
2024-02-02 23:47:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+ const openModalButton = document.getElementById('openModal');
+  const modal = document.getElementById('myModal');
+
+  openModalButton.addEventListener('click', openModal);
+
+  function openModal() {
+    modal.style.display = 'flex';
+  }
+
+  window.onclick = function(event) {
+    if (event.target === modal) {
+      closeModal();
+    }
+  }
+
+  function closeModal() {
+    modal.style.display = 'none';
+  }
